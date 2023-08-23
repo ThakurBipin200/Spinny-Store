0934bc1eff99e826d59f06f8d4bcf19dcc033b2a
Bipin kumar <76779132+ThakurBipin200@users.noreply.github.com>
2023-08-23 13:01:21 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,12 @@
 echo "BUILD START"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
+pip install wheel setuptools pip --upgrade
+pip3 install wheel setuptools pip --upgrade
+
+python -m pip install wheel setuptools pip --upgrade
+python3 -m pip install wheel setuptools pip --upgrade
+py -m pip install wheel setuptools pip --upgrade
+
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
+
